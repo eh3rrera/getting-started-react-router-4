c98333fed7124fd34a7b809bbf752c880099d791
Esteban Herrera <estebanhb2@gmail.com>
2018-10-22 17:06:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import {
-  Link,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
-
-import Home from './components/Home';
-import About from './components/About';
-import Users from './components/Users';
-
 class App extends Component {
   render() {
     return (
@@ -26,29 +15,12 @@ class App extends Component {
         <nav className="navbar-primary">
           <ul className="navbar-primary-menu">
             <li>
-              <Link to="/">
-                <span className="glyphicon glyphicon-th"></span><span className="nav-label">Home</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/about">
-                <span className="glyphicon glyphicon-th"></span><span className="nav-label">About</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/users">
-                <span className="glyphicon glyphicon-th"></span><span className="nav-label">Users</span>
-              </Link>
+                <a href="#"><span className="glyphicon glyphicon-th"></span><span className="nav-label">Home</span></a>
             </li>
           </ul>
         </nav>
         <div className="main-content">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/users" component={Users} />
-            <Redirect to="/" />
-          </Switch>
+          <h1>Hello World!</h1>
         </div>
       </div>
     );
