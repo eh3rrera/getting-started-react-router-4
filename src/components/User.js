61527e615463669f6f7f6e0502c00815d2f8354f
Esteban Herrera <estebanhb2@gmail.com>
2018-10-22 17:16:27 -0500

    first commit

--- src/components/User.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const User = ({ match }) => (
-  <h3>User with ID {match.params.id}</h3>
-);
-
-export default User;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 import {
-  Link,
-  Route,
-  Switch
+  Link
 } from 'react-router-dom';
-
-import User from './User';
 
 const Users = ({ match }) => (
   <div>
     {
-      getUserList().map(user =>  
+      getUserList().map(user => 
         <h2 key={user.id}>
           <Link to={`${match.url}/${user.id}`}>
             User {user.id}
@@ -19,23 +15,15 @@ const Users = ({ match }) => (
         </h2>
       )
     }
-    <Switch>
-      <Route
-        exact
-        path={match.url}
-        render={() => <h3>Please select a user from the list</h3>}
-      />
-      <Route path={`${match.url}/:id(\\d+)`} component={User} />
-    </Switch>
   </div>
 );
 
 const getUserList = () => {
- const list = [];
+  const list = [];
   for(let i = 1; i <= 5; i++) {
     list.push({id: i})
   }
-  return list;  
+  return list;
 }
 
 export default Users;
